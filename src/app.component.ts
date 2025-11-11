97d50939f139e5f91198335c6189a24195f392fe
Jojjeboy <karnstamjohannes@gmail.com>
2025-11-11 11:16:44 +0100

    basic folder structure

--- src/app.component.ts
+++ src/app.component.ts
@@ -5,6 +5,6 @@ import { RouterModule } from '@angular/router';
     selector: 'app-root',
     standalone: true,
     imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`
+    templateUrl: './app.components.html'
 })
 export class AppComponent {}
