27638e457418f8167d286096a55c838419dd2609
Jojjeboy <karnstamjohannes@gmail.com>
2025-11-14 19:44:16 +0100

    Initial Dialog

--- src/app/pages/component/list-completed-exercise-component/list-completed-exercise-component.ts
+++ src/app/pages/component/list-completed-exercise-component/list-completed-exercise-component.ts
@@ -1,19 +1,25 @@
 import { CompletedExerciseModel } from '@/pages/models/completedExercise.model';
 import { Component, Input } from '@angular/core';
-import { List } from '../List';
 import { CommonModule } from '@angular/common';
+import { Dialog } from 'primeng/dialog';
+import { List } from '../List';
+
 
 @Component({
   selector: 'app-list-completed-exercise-component',
-  imports: [CommonModule],
+  imports: [CommonModule, Dialog],
   templateUrl: './list-completed-exercise-component.html',
   styleUrl: './list-completed-exercise-component.scss'
 })
 export class ListCompletedExerciseComponent extends List {
 
+  visible: boolean = false;
   @Input() completedExercises?: CompletedExerciseModel[] | null;
 
   
+  showDialog(completedExercise: CompletedExerciseModel) {
+      this.visible = true;
+  }
 
   showPopup(passsedVal: string) {
     alert(passsedVal);
--- src/app/pages/component/list-exercise-component/list-exercise-component.ts
+++ src/app/pages/component/list-exercise-component/list-exercise-component.ts
@@ -2,18 +2,23 @@ import { ExerciseModel } from '@/pages/models/exercise.model';
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { List } from '../List';
+import { Dialog } from 'primeng/dialog';
 
 @Component({
   selector: 'app-list-exercise-component',
-  imports: [CommonModule],
+  imports: [CommonModule, Dialog],
   templateUrl: './list-exercise-component.html',
   styleUrl: './list-exercise-component.scss'
 })
 export class ListExerciseComponent extends List{
 
-
   @Input() exercises?: ExerciseModel[];
-
+  visible: boolean = false;
+  
+  
+  showDialog(exercise: ExerciseModel) {
+      this.visible = true;
+  }
 
 
 
